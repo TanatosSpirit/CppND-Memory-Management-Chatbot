adb7cb9d635b6d6507793ecc9011cf4faa4d66e5
Andrei <lytovandrei@gmail.com>
2023-08-15 17:12:50 +0300

    Task 0

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,8 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    // GraphNode is not owner this resource
+    // delete _chatBot;
 
     ////
     //// EOF STUDENT CODE
